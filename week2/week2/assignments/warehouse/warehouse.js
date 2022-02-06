c7d9a6b7362ed6ad48969a095928e49929001d38
JessSmith44 <91652487+JessSmith44@users.noreply.github.com>
2022-02-06 01:21:10 -0700

    Add files via upload

--- week2/week2/assignments/warehouse/warehouse.js
+++ week2/week2/assignments/warehouse/warehouse.js
@@ -21,19 +21,91 @@ const parts = [
     { partNbr: 'WEYPU3Z', partDescr: 'Mumpenflipper', aisle: 'E3', qty: 1}
 
 ]
+var ordersList = document.querySelector("#detailsList");
 
 // list of each part number and qty for check-off in the "detailsList" element
+//make for loop to assign checkbox and append
+
+parts.forEach(myfunction);
+
+function myfunction(item, index){
+    var checkBoxes = document.createElement("input");
+        checkBoxes.type = "checkbox";
+        checkBoxes.id = "box" + index;
+        ordersList.appendChild(checkBoxes);
+
+    var boxLabel = document.createElement("label");
+        boxLabel.setAttribute("for", "box" + index);
+        boxLabel.innerHTML = `${item.qty} ${item.partNbr} ${item.partDescr} <br>`;
+        ordersList.appendChild(boxLabel);
+}
 
 // if parts requiring special handling exist (in aisle B3), list of items needing 
 // special packaging in the "specialPackaging" element, else remove element
 
+var specialBox = document.querySelector("#specialPackaging");
+var handleWithCare = parts.filter(function(element, index) {
+    return parts[index].aisle === "B3";
+});
+
+if (handleWithCare.length !== 0) {
+    handleWithCare.forEach(function(element, index) {
+        let specialList = document.createElement("p");
+        specialList.textContent = `Item: ${handleWithCare[index].partNbr} / Qty: ${handleWithCare[index].qty}`;
+        specialBox.appendChild(specialList);
+    })
+} else {
+    specialPackaging.remove();
+}
+
+
 // if hazardous parts exist (in aisle J4), let employee know in the "hazardousMaterials"
 // element and remind them to get gloves, else remove element
+
+let hazardousMaterials = document.querySelector("#hazardousMaterials");
+let hazardous = parts.some(function(element, index) {
+    return parts[index].aisle === "J4";
+})
+
+if (hazardous !== true) {
+    hazardousMaterials.remove();
+} else {
+    let noteOfCaution = document.createElement("p");
+    noteOfCaution.textContent = "Gloves are required!";
+    hazardousMaterials.appendChild(noteOfCaution);
+}
 
 // if all items in the order are small parts (aisle H1), then let employee know that they should take 
 // a basket and go dirctly to aisle H1
 
+let smallParts = document.querySelector("#smallItemsOnly");
+let smallItems = parts.every(function(element, index) {
+    return parts[index].aisle === "H1";
+})
+if (smallItems !== true) {
+    smallParts.remove();
+} else {
+    let smallPartsInfo = document.createElement("p");
+    smallPartsInfo.textContent = "Small items: take a basket and go directly to asile H1";
+    smallParts.appendChild(smallPartsInfo);
+}
+
 // if there are large items (anthing in aisles S, T, or U), then let the employee know in the "forkiftNeeded"
 // element that they will need to reserve a forklift, else remove the element
 
+let forkLiftNeeded = document.querySelector("#forkLiftNeeded");
+let largeParts = parts.find(function(element, index) {
+    return parts[index].aisle === "S" || parts[index].aisle === "T" || parts[index].aisle === "U";
+})
+if (largeParts === undefined) {
+
+} else {
+    let forkliftNote = document.createElement("p");
+    forkliftNote.textContent = "Large items: Forklift required!";
+    largeParts.appendChild(forkliftNote);
+}
+
 // sum up the total number of parts and append that number to the text already in "totalItems" element
+// document.getElementById("totalItems").innerHTML = parts.reduce();
+let total = parts.reduce((total, part) => total + part.qty, 0);
+document.querySelector('#totalItems').textContent += ': ' + total;
